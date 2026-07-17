833186a6eebf7b9f710100591e41bcbd25bdfe1c
prateeksolanki814-commits <prateeksolanki814@gmail.com>
2026-07-17 15:50:53 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -2,13 +2,12 @@ import base64
 from datetime import datetime
 
 def get_table_download_link(df, filename_prefix, file_type="csv"):
-    """Generates a link to download a dataframe as CSV or JSON."""
     if file_type == "csv":
         data = df.to_csv(index=False)
         b64 = base64.b64encode(data.encode()).decode()
-        href = f'<a href="data:file/csv;base64,{b64}" download="{filename_prefix}_{datetime.now().strftime("%Y%m%d")}.csv" class="download-btn">📥 Download CSV File</a>'
+        href = f'<a href="data:file/csv;base64,{b64}" download="{filename_prefix}_{datetime.now().strftime("%Y%m%d")}.csv">📥 Download CSV File</a>'
     else:
         data = df.to_json(orient="records")
         b64 = base64.b64encode(data.encode()).decode()
-        href = f'<a href="data:file/json;base64,{b64}" download="{filename_prefix}_{datetime.now().strftime("%Y%m%d")}.json" class="download-btn">📥 Download JSON File</a>'
+        href = f'<a href="data:file/json;base64,{b64}" download="{filename_prefix}_{datetime.now().strftime("%Y%m%d")}.json">📥 Download JSON File</a>'
     return href
